2a722d742946eeb65d9c4c293956966bbab4f8f5
VioletaNaboy <vnaboishchykova@gmail.com>
2023-07-10 17:47:23 +0200

    test-3

---
+++ test-3.bash
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/shell
+n=$1
+if [ $n -ge 10 ]; then 
+    echo "Great, now move on to tricks"
+else 
+    echo "Keep at it until you get it"
+fi
