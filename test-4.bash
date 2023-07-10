dde0838b1236ac008d48cfc1d265d946482c188d
VioletaNaboy <vnaboishchykova@gmail.com>
2023-07-10 17:54:53 +0200

    test-4

---
+++ test-4.bash
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/582cd9033c1acf1d45000052/train/shell
+#!/bin/bash
+
+countToTwenty() {
+  # Your Code Here...
+for ((i=1; i<=20; i++))
+do
+    echo "Count: $i"
+done
+}
+
+countToTwenty
